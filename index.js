a88c647e873bc1cd23f6d94a365963124013bc07
Pairing Station 1 <antonio.yip@forsitenz.com>
2017-08-09 15:49:40 +1200

    have to wait for pg to be redy

--- index.js
+++ index.js
@@ -3,35 +3,34 @@
 
     const winston = require("winston");
     const { Client } = require('pg');
-    let client;
+    let client = null;
 
-    ( async function(){
-
-        // Self Executing function to
-        // connect to postgres ONCE
-        // Then the client will be reused;
-
-        winston.info( `DATABASE_URI: ${process.env.DATABASE_URI}`);
-        winston.info( `PGHOST:       ${process.env.PGHOST}`);
-        winston.info( `PGUSER:       ${process.env.PGUSER}`);
-        winston.info( `PGDATABASE:   ${process.env.PGDATABASE}`);
-        winston.info( `PGPASSWORD:   ${process.env.PGPASSWORD}`);
-        winston.info( `PGPORT:       ${process.env.PGPORT}`);
+    async function once(){
 
         try {
 
-            client = new Client( process.env.DATABASE_URI );
+            if ( !client ) {
 
-            await client.connect();
+                winston.info( `DATABASE_URI: ${process.env.DATABASE_URI}`);
+                winston.info( `PGHOST:       ${process.env.PGHOST}`);
+                winston.info( `PGUSER:       ${process.env.PGUSER}`);
+                winston.info( `PGDATABASE:   ${process.env.PGDATABASE}`);
+                winston.info( `PGPASSWORD:   ${process.env.PGPASSWORD}`);
+                winston.info( `PGPORT:       ${process.env.PGPORT}`);
+
+                client = new Client( process.env.DATABASE_URI );
+
+                await client.connect();
+            }
 
         } catch( connection_err ){
 
             winston.error( `CONNECTION ERROR: ${connection_err}` );
 
+            throw connection_err;
         }
 
-
-    })();
+    }
 
 
     module.exports = async function( SQL_QUERY ) {
@@ -39,6 +38,8 @@
         winston.info( SQL_QUERY );
 
         try {
+
+            await once();
 
             return await client.query( SQL_QUERY ); // If query successful it MUST ALWAYS return an array.
 
